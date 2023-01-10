3f4f4d49b076acca0e0a8f24d37a9521a520a615
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2023-01-10 18:03:59 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
