7f4c036e2e8889842cda56176402054e4c87e1c5
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2023-01-11 06:28:29 +0200

    add productCard component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <script lang="ts">
 import { RouterView } from "vue-router";
+import ProductCard from "./components/ProductCard.vue";
+export default {
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      product: {
+        id: 1,
+        title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        price: 109.95,
+        description:
+          "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        category: "men's clothing",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        rating: { rate: 3.9, count: 120 },
+      },
+    };
+  },
+  methods: {
+    addToCart(id: number) {
+      // add to cart implementation
+    },
+  },
+};
 </script>
 
 <template>
+  <ProductCard :product="product" @add-to-cart="addToCart" />
   <RouterView />
 </template>
 
-<style scoped>
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-</style>
+<style scoped></style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="max-w-xs text-center">
+    <img class="w-[40%] mx-auto" :src="product.image" :alt="product.title" />
+    <h2 class="text-lg mt-3">{{ product.title }}</h2>
+    <p class="mt-3 mb-3">{{ "$" + product.price }}</p>
+    <p class="mb-5">{{ product.description }}</p>
+    <button
+      class="bg-slate-900 text-white pt-3 pb-3 pr-3 pl-3"
+      @click="$emit('addToCart', product.id)"
+    >
+      Add to cart
+    </button>
+  </section>
+</template>
+
+<script lang="ts">
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: Rate;
+}
+
+interface Rate {
+  rate: number;
+  count: number;
+}
+
+import type { PropType } from "vue";
+
+export default {
+  props: {
+    product: {
+      type: Object as PropType<Product>,
+      required: true,
+    },
+  },
+  emits: {
+    addToCart(id: number) {
+      return id;
+    },
+  },
+};
+</script>
