c36a8719880df91a8242d143b0d268830744ddc0
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2023-01-11 09:05:02 +0200

    add BaseComponent

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="btn">
+    {{ text }}
+    <slot name="icon"></slot>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
